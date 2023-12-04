2727b86f3f62197f0dfbc6188cfbaa6ddc9450ee
Ting Ting Chen Zeng <tin2294@gmail.com>
2023-12-04 00:21:42 -0500

    initial table of vehicles

--- wow/views.py
+++ wow/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from wow.models import Vehicle, Vclass
 
 
 # Create your views here.
@@ -10,11 +11,16 @@ def index(request):
 def bookings(request):
     return render(request, 'bookings.html')
 
+
 def vehicles(request):
-    return render(request, 'vehicles.html')
+    vehicles_queryset = Vehicle.objects.all().values('make', 'model', 'year', 'classid__class_name', 'classid__daily_rate', 'classid__daily_mileage', 'classid__overage_rate')
+    vehicles = list(vehicles_queryset)
+    return render(request, 'vehicles.html', {'vehicles': vehicles})
+
 
 def vehicle_details(request, vehicle_id):
     return render(request, 'vehicle_details.html', {'vehicle_id': vehicle_id})
+
 
 def booking_details(request, service_id):
     return render(request, 'booking_details.html', {'service_id': service_id})
