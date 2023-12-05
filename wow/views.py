439328cf91d0f583e7e9023ef570be971cadc534
Mike Zaharchenko <mz2433@nyu.edu>
2023-12-05 13:03:47 -0500

    Removed all pycache and .idea files

--- wow/views.py
+++ wow/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Comment to commit
 # Create your views here.
 def index(request):
     return HttpResponse("WOW Index")
