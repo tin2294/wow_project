0fb32f4da3467264867545d8d47b5587c08d52aa
Ting Ting Chen Zeng <tin2294@gmail.com>
2023-12-09 10:41:22 -0500

    Revert "Add files via upload"
    This reverts commit 7c9d0def1400faaf9ee49e4c6d54c1105a64b93f.

--- urls.py
+++
@@ -1,8 +0,0 @@
-from wow import views
-
-
-path('order/list/', order.order_list),
-path('order/add/', order.order_add),
-path('order/delete/', order.order_delete),
-path('order/detail/', order.order_detail),
-path('order/edit/', order.order_edit),
