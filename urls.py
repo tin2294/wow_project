7c9d0def1400faaf9ee49e4c6d54c1105a64b93f
Ruanxiao-01 <81853607+Ruanxiao-01@users.noreply.github.com>
2023-12-09 10:29:29 -0500

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,8 @@
+from wow import views
+
+
+path('order/list/', order.order_list),
+path('order/add/', order.order_add),
+path('order/delete/', order.order_delete),
+path('order/detail/', order.order_detail),
+path('order/edit/', order.order_edit),
